8a88194b7de77d04d0babcebe4e94894b936b177
georgie-saunders <georgie.s2@hotmail.com>
2022-06-20 12:35:10 +1000

    update css

--- script.js
+++
@@ -1,7 +0,0 @@
-function rsvp() {
-  let response = document.querySelector("button");
-  response.innerHTML = "Thank you! You have RSVP'd.";
-}
-
-let rsvpButton = document.querySelector("button");
-rsvpButton.addEventListener("click", rsvp);
